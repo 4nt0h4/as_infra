54ceb4bc1af40a1e7f5514122f967e7c8626dd2c
Anton Sergeev <stalkrmain@gmail.com>
2021-04-07 16:02:24 +0300

    Create immutable VM puma-server

--- packer/files/install_puma.sh
+++ packer/files/install_puma.sh
@@ -7,16 +7,21 @@ git clone -b monolith https://github.com/express42/reddit.git
 
 cd /home/appuser/reddit && bundle install
 
-echo '[Unit]
-Description=Puma-server
+echo "[Unit]
+Description=Puma HTTP Server
+After=network.target
 
 [Service]
-ExecStart=/usr/local/bin/puma -d --dir /home/appuser/reddit
+Type=simple
+User=appuser
+WorkingDirectory=/home/appuser/reddit
+ExecStart=/bin/bash -lc 'puma'
+Restart=always
 
 [Install]
 WantedBy=multi-user.target
-' > /etc/systemd/system/rd-puma.service
-
-chmod 664 /etc/systemd/system/rd-puma.service
+" > /etc/systemd/system/puma.service
 
 systemctl daemon-reload
+
+systemctl enable puma.service
